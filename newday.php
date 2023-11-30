bcef0624f9d2d527ee5b1f91fb66b941a6bd2616
Guillaume <guillaume.turpin45@gmail.com>
2023-11-30 14:33:16 +0100

    :sparkles: Make the generator script

---
+++ newday.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * File which must be called to create a new day solution by CLI
+ */
+
+use App\Services\NewDayGenerator;
+
+if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
+	require_once __DIR__ . '/vendor/autoload.php';
+} else {
+	die;
+}
+
+if ( ! isset( $argv[1] ) ) {
+	die( 'You must provide a day number.' );
+}
+
+$day_number = (int) $argv[1];
+NewDayGenerator::generate( $day_number );
---
+++ src/Services/NewDayGenerator.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Services;
+
+/**
+ * This class will generate the files for a new day
+ */
+final class NewDayGenerator {
+	/**
+	 * Generate the files for a new day
+	 *
+	 * @param integer $day_number The number of the day to generate
+	 */
+	public static function generate( int $day_number ): void {
+		$root_dir = dirname( __DIR__, 2 );
+
+		// Create the data directory
+		self::maybe_mkdir( $root_dir . '/data/day' . $day_number );
+		self::create_file( $root_dir . '/data/day' . $day_number . '/sample.txt' );
+		self::create_file( $root_dir . '/data/day' . $day_number . '/input_part1.txt' );
+		self::create_file( $root_dir . '/data/day' . $day_number . '/input_part2.txt' );
+
+		// Create the solution directory
+		$solution_stub = self::get_solution_stub();
+		$solution_stub = preg_replace( '/\bDummyDayNumber\b/', $day_number, $solution_stub );
+		$solution_stub = preg_replace( '/\bDayDummyDayNumber\b/', 'Day' . $day_number, $solution_stub );
+		self::maybe_mkdir( $root_dir . '/src/Solutions/Day' . $day_number );
+		self::create_file( $root_dir . '/src/Solutions/Day' . $day_number . '/Day' . $day_number . '.php', $solution_stub );
+	}
+
+	/**
+	 * Create a file if it does not exist
+	 *
+	 * @param string $filename The name of the file to create
+	 * @param string $content  The content of the file
+	 *
+	 * @return void
+	 */
+	protected static function create_file( string $filename, string $content = '' ): void {
+		if ( ! file_exists( $filename ) ) {
+			file_put_contents( $filename, $content );
+		}
+	}
+
+	/**
+	 * Create a directory if it does not exist
+	 *
+	 * @param string $dirname The name of the directory to create
+	 *
+	 * @return void
+	 */
+	protected static function maybe_mkdir( string $dirname ): void {
+		if ( ! file_exists( $dirname ) ) {
+			mkdir( $dirname );
+		}
+	}
+
+	/**
+	 * Get the stub for the solution file
+	 *
+	 * @return string
+	 */
+	protected static function get_solution_stub(): string {
+		// Get content of the Day.stub
+		$stub = file_get_contents( dirname( __DIR__, 2 ) . '/src/stubs/Day.stub' );
+
+		return $stub;
+	}
+}
